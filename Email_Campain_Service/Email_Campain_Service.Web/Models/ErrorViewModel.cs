a871be6982b4a3aeccd559f14ca256e7ac5c9670
AKM Rafiqul Alam <akmrafiq450@gmail.com>
2020-02-23 09:43:46 +0600

    Project_Initialization

---
+++ Email_Campain_Service/Email_Campain_Service.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Email_Campain_Service.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
